a835ddbb49344a27ce11f5d1efacecc8756dc188
haongtrinh21 <trinhhoang2001bn@gmail.com>
2022-05-22 23:11:18 +0700

    bai2

--- tuan1/src/main/java/my/bai2/Bai2.java
+++ tuan1/src/main/java/my/bai2/Bai2.java
@@ -1,23 +1,38 @@
 package my.bai2;
 
 import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 public class Bai2 {
     public static Set<Integer> random(int n) {
-        Set<Integer> rndList = new HashSet<>();
+        Set<Integer> rndList = new TreeSet<>();
         Random random = new Random();
         int tmp;
         while (rndList.size() < n) {
-            tmp = (random.nextInt() % Integer.MAX_VALUE - Integer.MIN_VALUE + 1) + Integer.MIN_VALUE;
+            tmp = (random.nextInt() % Integer.MIN_VALUE - Integer.MIN_VALUE) + Integer.MIN_VALUE;
             rndList.add(tmp);
         }
         return rndList;
     }
 
     public static void main(String[] args) {
-        Set<Integer> set = random(1000);
         int k = new Scanner(System.in).nextInt();
-
-
+        long start = System.currentTimeMillis();
+        Set<Integer> set = random(1000000);
+        Map<Integer, Integer> res = new HashMap<>();
+        int i = 0, a, b;
+        for (Integer s : set) {
+            if (i > k/2) break;
+            a = s.intValue();
+            b = k - a;
+            if (set.contains(b)) {
+                res.put(a, b);
+            }
+            i++;
+        }
+        long end = System.currentTimeMillis();
+        System.out.println(end - start + "ms");
+        System.out.println(res);
     }
 }
